2a8a4e6ae0832897b04ef25a6defdf3593700ae0
Dexter Aparicio <dexter@newlogic.io>
2020-11-11 18:10:20 +0800

    Add ipaddr tracking; Parse qparams; Add local.properties

--- src/main/java/com/example/eigenfield/DemoApplication.java
+++ src/main/java/com/example/eigenfield/DemoApplication.java
@@ -5,8 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 @SpringBootApplication
@@ -27,9 +32,14 @@ public class DemoApplication {
 		ctx = SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/visit")
-	public long visit()
-	{
+    @GetMapping(path={"/visit", "/visit/{ipaddr}"})
+    public long visit(@PathVariable(required = false, name = "ipaddr") String ipaddr,
+                      @RequestParam(required = false) Map<String, String> qparams)
+    {
+        qparams.forEach((a, b) -> {
+            System.out.println(String.format("%s -> %s",a,b));
+        });
+
         DemoApplication demo = ctx.getBean(DemoApplication.class);
         Jedis jedis = new Jedis(demo.redis_host, demo.redis_port);
         if (!demo.redis_pass.isEmpty()) {
@@ -37,7 +47,11 @@ public class DemoApplication {
         }
         jedis.connect();
         long count = jedis.incr("resumeviews");
+        if (ipaddr != null) {
+            String ldt = LocalDateTime.now().toString();
+            jedis.lpush(ipaddr, ldt);
+        }
         jedis.disconnect();
         return count;
-	}
+    }
 }
